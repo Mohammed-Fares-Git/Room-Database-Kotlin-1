a8b8da8e0ed0863cca5c7db040db6d0ec9c5debc
Mohammed-Fares-Dev <mohammedfarestech@gmail.com>
2023-11-10 13:58:28 +0100

    implement appartement dao

--- app/src/main/java/com/example/room_database_tuto/dao/AppartementDao.kt
+++ app/src/main/java/com/example/room_database_tuto/dao/AppartementDao.kt
@@ -1,4 +1,21 @@
 package com.example.room_database_tuto.dao
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.room_database_tuto.db.AppartementDatabase
+import com.example.room_database_tuto.model.Appartement
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface AppartementDao {
+    @Query("SELECT * FROM ${AppartementDatabase.TABLE_NAME}")
+    fun getAll(): Flow<List<Appartement>>
+
+    @Insert
+    suspend fun insertAll(vararg appartement: Appartement)
+
+    @Delete
+    suspend fun delete(appartement: Appartement)
 }
