0eb660fb096d76737e4ef4fb81381317eb092a31
Mohammed-Fares-Dev <mohammedfarestech@gmail.com>
2023-11-10 14:23:43 +0100

    create appartement dao

--- app/src/main/java/com/example/room_database_tuto/model/Appartement.kt
+++ app/src/main/java/com/example/room_database_tuto/model/Appartement.kt
@@ -1,5 +1,20 @@
 package com.example.room_database_tuto.model
 
 
-class Appartement {
-}
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.room_database_tuto.db.AppartementDatabase
+import java.io.Serializable
+
+@Entity(tableName = AppartementDatabase.TABLE_APPARTEMENTS_NAME)
+data class Appartement (
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = AppartementDatabase.ID_COLUMN_NAME)
+    val id: Int,
+    @ColumnInfo(name = AppartementDatabase.OFFER_COLUMN_NAME) val offer: String,
+    @ColumnInfo(name = AppartementDatabase.SURFACE_COLUMN_NAME) val surface: Int,
+    @ColumnInfo(name = AppartementDatabase.PARKING_COLUMN_NAME) val avecParking: Boolean,
+    @ColumnInfo(name = AppartementDatabase.IMG_COLUMN_NAME) val image: String
+): Serializable
