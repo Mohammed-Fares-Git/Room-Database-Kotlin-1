ffd7b0c88e42dae193f1506f98d0f29ef649e815
Mohammed-Fares-Dev <mohammedfarestech@gmail.com>
2023-11-10 13:53:07 +0100

    create appartement entity

--- app/src/main/java/com/example/room_database_tuto/db/AppartementDatabase.kt
+++ app/src/main/java/com/example/room_database_tuto/db/AppartementDatabase.kt
@@ -1,4 +1,12 @@
 package com.example.room_database_tuto.db
 
 class AppartementDatabase {
+    companion object {
+        const val TABLE_NAME = "appatements_table"
+        const val ID_COLUMN_NAME = "_id"
+        const val OFFER_COLUMN_NAME = "_offer"
+        const val PARKING_COLUMN_NAME = "_avec_parking"
+        const val SURFACE_COLUMN_NAME = "_surface"
+        const val IMG_COLUMN_NAME = "_img"
+    }
 }
--- app/src/main/java/com/example/room_database_tuto/model/Appartement.kt
+++ app/src/main/java/com/example/room_database_tuto/model/Appartement.kt
@@ -1,5 +1,17 @@
 package com.example.room_database_tuto.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.room_database_tuto.db.AppartementDatabase
 
-class Appartement {
-}
+@Entity(tableName = AppartementDatabase.TABLE_NAME)
+data class Appartement (
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = AppartementDatabase.ID_COLUMN_NAME)
+    val id: Int,
+    @ColumnInfo(name = AppartementDatabase.OFFER_COLUMN_NAME) val offer: String,
+    @ColumnInfo(name = AppartementDatabase.SURFACE_COLUMN_NAME) val surface: Int,
+    @ColumnInfo(name = AppartementDatabase.PARKING_COLUMN_NAME) val avecParking: Boolean,
+    @ColumnInfo(name = AppartementDatabase.IMG_COLUMN_NAME) val image: String
+    )
